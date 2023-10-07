7b242d5d8d9ce2bacb66d90c3f3067ccc4b5578e
d_butov <danyil.butov@gmail.com>
2023-10-07 20:16:51 +0100

    feature: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+
+// Little helpers ...
+const url = (name: string, wrap = false) =>
+  `${
+    wrap ? "url(" : ""
+  }https://awv3node-homepage.surge.sh/build/assets/${name}.svg${
+    wrap ? ")" : ""
+  }`;
+
+export default function App() {
+  return (
+    <div>
+      <Parallax pages={2}>
+        <ParallaxLayer
+          offset={0}
+          speed={0.2}
+          factor={2}
+          style={{ backgroundColor: "#171717" }}
+        />
+        <ParallaxLayer
+          offset={1}
+          speed={1}
+          factor={1.5}
+          style={{ backgroundColor: "#87BCDE" }}
+        />
+        <ParallaxLayer
+          offset={0}
+          speed={0}
+          factor={2}
+          style={{
+            backgroundImage: url("stars", true),
+            backgroundSize: "cover",
+          }}
+        />
+        <ParallaxLayer
+          offset={0.5}
+          speed={-0.5}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            pointerEvents: "none",
+          }}
+        >
+          <img src={url("earth")} style={{ width: "35%" }} alt="govno" />
+        </ParallaxLayer>
+      </Parallax>
+    </div>
+  );
+}
