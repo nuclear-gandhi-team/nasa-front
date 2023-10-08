4bde0d8c9ab75aa90d85b425de980d8e226e6bf5
d_butov <danyil.butov@gmail.com>
2023-10-08 20:01:35 +0100

    feature: implement register

--- src/feature/EnrollModal.tsx
+++ src/feature/EnrollModal.tsx
@@ -1,4 +1,5 @@
-import { Dispatch, FC, SetStateAction } from 'react';
+import { ChangeEvent, Dispatch, FC, SetStateAction, useState } from 'react';
+import axios from 'axios';
 
 import Button, { ButtonSize } from '../common/Button';
 import Input from '../common/Input';
@@ -11,7 +12,51 @@ export interface EnrollModalProps {
   marker: string | undefined;
 }
 
-const EnrollModal: FC<EnrollModalProps> = ({ openModal, setOpenModal }) => {
+interface UserData {
+  username: string;
+  email: string;
+  password: string;
+  coordinates: string | undefined;
+}
+
+const EnrollModal: FC<EnrollModalProps> = ({
+  openModal,
+  setOpenModal,
+  marker,
+}) => {
+  const [userData, setUserData] = useState<UserData>({
+    username: '',
+    email: '',
+    password: '',
+    coordinates: marker,
+  });
+  const [description, setDescription] = useState<string>('');
+  const handleInputChange = (
+    e: ChangeEvent<HTMLInputElement>,
+    fieldName: keyof UserData,
+  ) => {
+    const { value } = e.target;
+    setUserData(prevData => ({
+      ...prevData,
+      [fieldName]: value,
+    }));
+  };
+
+  const handleRegister = () => {
+    axios
+      .post(
+        'https://firesense.azurewebsites.net/api/Account/register',
+        userData,
+      )
+      .then(response => {
+        console.log(response.data);
+        setOpenModal(false);
+      })
+      .catch(error => {
+        setDescription(error.error);
+      });
+  };
+
   return (
     <>
       {openModal && (
@@ -26,12 +71,16 @@ const EnrollModal: FC<EnrollModalProps> = ({ openModal, setOpenModal }) => {
                 htmlFor="username"
                 id="username"
                 label="Username"
+                value={userData.username}
+                onChange={e => handleInputChange(e, 'username')}
               />
               <Input
                 placeholder="example@gmail.com"
                 htmlFor="email"
                 id="email"
                 label="Email"
+                value={userData.email}
+                onChange={e => handleInputChange(e, 'email')}
               />
               <Input
                 placeholder="*********"
@@ -39,14 +88,17 @@ const EnrollModal: FC<EnrollModalProps> = ({ openModal, setOpenModal }) => {
                 id="password"
                 label="Password"
                 type="password"
+                value={userData.password}
+                onChange={e => handleInputChange(e, 'password')}
               />
             </div>
-            <div className="flex flex-col gap-2 m-5 w-full items-end">
+            <div className="flex flex-col gap-3 m-5 w-full items-end">
               <Button
                 className="text-md w-full inline-block font-bold bg-gray-950 px-4 py-2 leading-none rounded-lg text-gray-200 hover:bg-gray-900 disabled:border-gray-700 disabled:text-gray-600"
                 text="Register"
                 size={ButtonSize.SMALL}
-                onClick={() => setOpenModal(false)}
+                onClick={handleRegister}
+                description={description}
               />
               <Link
                 className="text-xs text-gray-100 hover:text-sky-400"
