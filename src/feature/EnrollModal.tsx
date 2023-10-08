ede5c9fd2b9320a45d0980de7e482977d8e69be0
HJK <89708817+HJyup@users.noreply.github.com>
2023-10-08 19:25:59 +0100

    Feature/main page (#13)
    * components and feature: implement Modal, EnrollModal and Input component
    
    * fix: remove unnecessary styles
    
    * feature: implement main page

--- src/feature/EnrollModal.tsx
+++ src/feature/EnrollModal.tsx
@@ -16,7 +16,10 @@ const EnrollModal: FC<EnrollModalProps> = ({ openModal, setOpenModal }) => {
     <>
       {openModal && (
         <Modal>
-          <div className="p-6 flex flex-col gap-3 h-96 w-96">
+          <p className="text-gray-400 text-xs">
+            We will check the location that you chose in maps
+          </p>
+          <div className="p-6 flex flex-col gap-3 h-96 w-96 items-center">
             <div className="flex w-full gap-4 flex-col">
               <Input
                 placeholder="defender123"
@@ -38,14 +41,15 @@ const EnrollModal: FC<EnrollModalProps> = ({ openModal, setOpenModal }) => {
                 type="password"
               />
             </div>
-            <div className="flex flex-col items-end gap-2 m-5">
+            <div className="flex flex-col gap-2 m-5 w-full items-end">
               <Button
+                className="text-md w-full inline-block font-bold bg-gray-950 px-4 py-2 leading-none rounded-lg text-gray-200 hover:bg-gray-900 disabled:border-gray-700 disabled:text-gray-600"
                 text="Register"
                 size={ButtonSize.SMALL}
                 onClick={() => setOpenModal(false)}
               />
               <Link
-                className="text-xs text-gray-400 hover:text-sky-400"
+                className="text-xs text-gray-100 hover:text-sky-400"
                 onClick={() => setOpenModal(false)}
               >
                 Close Modal
